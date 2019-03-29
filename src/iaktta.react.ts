c13b0eb4f07e9d2143e983514eb55d6991323aa4
Daniel Pihlstrom <sciolist.se@gmail.com>
2019-03-29 23:36:33 +0100

    component unmount issue

--- src/iaktta.react.ts
+++ src/iaktta.react.ts
@@ -14,16 +14,20 @@ function observer<RV extends (() => any) | (new() => any)>(component: RV): RV {
     return class ObserverWrapper extends (cls as any) {
       displayName = cls.name + '+iaktta';
       componentWillUnmount() {
-        super.componentWillUnmount();
         clearObserver(observers.get(this));
         observers.delete(this);
+        if (super.componentWillUnmount) {
+          super.componentWillUnmount();
+        }
       }
       render() {
         const oldRender = sfc ? component : super.render;
         if (!observers.has(this)) {
           observers.set(this, () => this.forceUpdate());
         }
-        return observe(observers.get(this), oldRender.bind(this, arguments));
+        const observer = observers.get(this);
+        clearObserver(observer);
+        return observe(observer, oldRender.bind(this, arguments));
       }
     } as any;
   }
