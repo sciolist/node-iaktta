3954d22db27fa832614edbe460975e78c59b906b
Daniel Pihlstrom <sciolist.se@gmail.com>
2019-04-01 16:46:05 +0200

    rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,35 @@
 import { terser } from 'rollup-plugin-terser';
-import { ts, dts } from "rollup-plugin-dts";
+import { ts, dts } from 'rollup-plugin-dts';
+import replace from 'rollup-plugin-replace';
 
-const minifier = process.env.MINIFY && terser({
-  compress: {
-    pure_getters: true,
-    module: true
-  },
-  module: true,
-  toplevel: true
-});
+const minifier =
+  process.env.MINIFY &&
+  terser({
+    compress: {
+      pure_getters: true,
+      module: true
+    },
+    output: {
+      beautify: process.env.BEAUTIFY=="1"
+    },
+    module: true,
+    toplevel: true
+  });
+
+const buildPlugins = [
+  minifier,
+  ts(),
+  replace({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+  })
+].filter(v => v);
 
 export function createConfiguration(name) {
   return [
     {
-      output: { dir: `./dist/${name}`, format: 'cjs' },
-      input: [
-        `./src/iaktta.ts`
-      ],
-      plugins: [ minifier, ts() ].filter(v => v),
+      output: { file: `./dist/${name}/iaktta.js`, format: 'cjs' },
+      input: `./src/iaktta.ts`,
+      plugins: buildPlugins,
       external: [name]
     },
     {
@@ -29,6 +41,4 @@ export function createConfiguration(name) {
   ];
 }
 
-export default [
-  ...createConfiguration('preact')
-]
+export default [...createConfiguration('preact')];
